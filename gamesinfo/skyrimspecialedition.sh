8116eb67583a17bf3b512de7aee6e565ce51af11
phantom <phantom.github@proton.me>
2026-01-31 14:54:59 -0700

    Update protontricks to include d3dcompiler_47

--- gamesinfo/skyrimspecialedition.sh
+++ gamesinfo/skyrimspecialedition.sh
@@ -4,7 +4,7 @@ game_gog_id=1711230643
 game_epic_id=""
 game_steam_subdirectory="Skyrim Special Edition"
 game_executable="SkyrimSELauncher.exe"
-game_protontricks=("xaudio2_7=native" "xact" "d3dcompiler_43" "vcrun2022")
+game_protontricks=("xaudio2_7=native" "xact" "d3dcompiler_43" "d3dcompiler_47" "vcrun2022")
 declare -A game_scriptextender_urls=(
 	["steam"]="https://skse.silverlock.org/beta/skse64_2_02_06.7z"
 	["gog"]=""
